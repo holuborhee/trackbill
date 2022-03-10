6f4fb0e0b7904920e2673c5a1301935a3b15732e
Samuel Lawal <samuellawal60@gmail.com>
2022-03-10 23:20:15 +0100

    Refacored my route and removed non usable pages₦

--- src/components/OutflowGroupCard.vue
+++ src/components/OutflowGroupCard.vue
@@ -2,35 +2,6 @@
 <section class="mb-8">
           <main>
             <ul class="flex justify-between">
-              <!-- <li class="flex-1 mr-4 card bg-blue-600 rounded">
-                <a href="#">
-                  <div class="flex flex-col p-12">
-                    <p
-                      class="
-                        font-display font-bold
-                        text-left
-                        font-body
-                        text-lg
-                        whitespace-no-wrap
-                        text-white
-                      "
-                    >
-                      Total Savings
-                    </p>
-                    <p
-                      class="
-                        font-display font-bold
-                        text-2xl text-left
-                        font-display
-                        text-xl text-white
-                      "
-                    >
-                      ₦71,000
-                    </p>
-                  </div>
-                </a>
-              </li> -->
-              <!-- <li class="flex-1 card mr-4 bg-blue-600 rounded">
                 <a href="#">
                   <div class="flex flex-col p-12">
                     <p
