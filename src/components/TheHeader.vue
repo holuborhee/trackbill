9e68c9a60aee32eccd49665d07cbd6486b22798f
timtech-wiz <60903808+timtech-wiz@users.noreply.github.com>
2022-06-09 00:48:54 +0100

    Landing Page Design (#8)
    * create single expense page and modal
    
    * Resolved bugs
    
    * make dynamic data on pages
    
    * resolve bugs
    
    * removed ref
    
    * add a login code page
    
    * Design-landing-page
    
    * design-landing-page
    
    * redesign-landing-page
    
    * use-convetional-named-routing
    
    * using-camel-case

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,20 +1,111 @@
 <template>
-    <div>
-        <nav class="bg-indigo-500 shadow-lg">
-                <div class="container mx-auto">
-                    <div class="sm:flex justify-around">
-                        <a href="/" class="text-white text-3xl font-bold p-3">TrackBill</a>
-                    
-                        <ul class=" flex text-grey-400 sm:self-center text-xl border-t sm:border-none">
-                                <li class="sm:inline-block">
-                                    <a href="/" class="p-3 hover:text-white" >Home</a>
-                                </li>
-                                <li class="sm:inline-block">
-                                <router-link :to="{name:'login'}" class="p-3 hover:text-white">Login</router-link>
-                                </li>
-                        </ul>
-                    </div>
-                </div>
-        </nav>
-    </div>
+  <div class="w-full h-full">
+    <nav
+      class="
+        flex
+        items-center
+        bg-gray-400
+        p-1
+        flex-wrap
+        shadow-md shadow-gray-500
+      "
+    >
+      <router-link
+        :to="{ name: 'Landing' }"
+        class="p-2 mr-4 inline-flex items-center"
+      >
+        <img
+          src="images/TrackBill-logo.png"
+          alt=""
+          width="60"
+          class="fill-current ml-16"
+        /><span
+          class="
+            text-2xl text-blue-900 text-center
+            font-bold
+            uppercase
+            tracking-wide
+          "
+          >TRACKBILL</span
+        ></router-link
+      >
+      <div
+        class="hidden top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto"
+      >
+        <div
+          class="
+            lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto
+            w-full
+            lg:items-center
+            items-start
+            flex-col
+            lg:h-auto
+            flex
+            justify-between
+            sm:self-center
+            text-xl
+            border-t
+            sm:border-none
+            text-gray-900
+          "
+        >
+          <div class="flex space-x-16 mr-80">
+            <a
+              href="#"
+              class="
+                px-3
+                py-2
+                rounded
+                text-gray-900
+                hover:bg-gray-900 hover:text-white
+              "
+            >
+              <span>About Us</span>
+            </a>
+            <a
+              href="#"
+              class="
+                px-3
+                py-2
+                rounded
+                text-gray-900
+                hover:bg-gray-900 hover:text-white
+              "
+            >
+              <span>Feautures</span>
+            </a>
+            <a
+              href="#"
+              class="
+                px-3
+                py-2
+                rounded
+                text-gray-900
+                hover:bg-gray-900 hover:text-white
+              "
+            >
+              <span>Contact</span>
+            </a>
+          </div>
+          <a
+             href="#"
+            class="
+              px-3
+              py-1
+              bg-gray-400
+              text-gray-900
+              items-center
+              justify-center
+              font-semibold
+              hover:bg-gray-900 hover:text-white
+              border border-blue-900
+              rounded-lg
+            "
+            ><span>Sign Up</span> </a>
+          
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
+
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
-
-import Login from "./views/Login.vue";
 import ExpenseCategory from "./views/ExpenseCategory.vue";
 import SingleExpenseCategory from "./views/SingleExpenseCategory.vue";
+import Landing from "./views/Landing.vue";
+ 
+
  
   
 
@@ -11,27 +12,22 @@ import SingleExpenseCategory from "./views/SingleExpenseCategory.vue";
 
 const routes =  [
                     {
-                       path: "/", 
-                       component: {template:"<p>Hello World</p>"}, 
-                       name:'home' 
-                    },
-                    { 
-                      path: "/login", 
-                      component: Login, 
-                      name:'login' 
+                      path: "/", 
+                      component: Landing, 
+                      name:'Landing' 
                     },
                     { 
                     path: "/expense-category", 
                     component: ExpenseCategory, 
-                    name:'expense-category' 
+                    name:'expenseCategory' 
                     },
 
                     { 
                       path: "/single-expense-category", 
                       component: SingleExpenseCategory, 
-                      name:'single-expense-category' 
+                      name:'singleExpenseCategory' 
                     },
-                ];
+                  ];
 const router = createRouter( {
      
         history: createWebHistory(),
